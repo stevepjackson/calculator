94f235183ba1f37785fbe4da2ffc8f50f0a3ed16
Steve Jackson <sjack7892@gmail.com>
2020-04-17 16:17:50 -0500

    basic setup

---
+++ server/public/scripts/client.js
@@ -0,0 +1,7 @@
+console.log('js ready');
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('jquery ready');
+}
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('Server started on port:', PORT);
+});
+
+
